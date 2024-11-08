675064ac3f5c70b8cfcccc1d5cbd1f7ebb7ecef3
Egor-Shi <gray-1592@mail.ru>
2024-11-08 16:13:28 +0300

    HomeWork_2_Mass_Cycle1

--- src/WhilePlusSum.java
+++ src/WhilePlusSum.java
@@ -4,18 +4,18 @@ public class WhilePlusSum {
     public static void main(String[] args) {
 
         Scanner a = new Scanner(System.in);
-int sum= 0;
-int i;
+        int sum = 0;
+        int i;
         System.out.println("Введите числа больше 0 : ");
-while (true){
-    i = a.nextInt();
-    if (i<0){
-        break;
-    }
-    sum = sum + i;
-}
+        while (true) {
+            i = a.nextInt();
+            if (i < 0) {
+                break;
+            }
+            sum = sum + i;
+        }
         System.out.println("Сумма : " + sum);
 
 
-        }
     }
+}
